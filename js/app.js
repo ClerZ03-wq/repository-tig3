ed5ae3171ed60beb0f327fad20f645131f961ce7
jamesfontanilla <jamesfontanilla.dev@proton.me>
2026-06-14 16:28:38 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let count = 0;
+
+const countDisplay = document.getElementById("count");
+const increaseBtn = document.getElementById("increaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+increaseBtn.addEventListener("click", () => {
+    count++;
+    countDisplay.textContent = count;
+});
+
+resetBtn.addEventListener("click", () => {
+    count = 0;
+    countDisplay.textContent = count;
+});
